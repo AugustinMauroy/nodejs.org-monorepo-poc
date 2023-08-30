d00fdcfb6b36c955d1ade16bebedf5a6034b8e73
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-08-30 23:07:55 +0200

    feat(api): introduce as CLI

--- apps/api-docs/app/api/[slug]/page.tsx
+++ apps/api-docs/app/api/[slug]/page.tsx
@@ -1,8 +1,11 @@
 // @ts-nocheck
-import getDoc from '@nodejs/core/dist/esm/lib/api-sync/getApiData';
+import path from 'node:path';
+import { compileMDX } from 'next-mdx-remote/rsc'
 import createMarkdownParser from '@nodejs/core/dist/esm/lib/api-sync/apiDocsTransformUtils';
+import { getContentBySlug } from '@nodejs/core/lib/contentManager';
 import type { FC } from 'react';
 import type { ApiDocsParams } from '@/types/params';
+
 
 type PageProps = {
     params: ApiDocsParams;
@@ -15,25 +18,43 @@ const Page: FC<PageProps> = async ({ params }) => {
         params.slug = params.slug.replace(/\.html$/, '');
     }
 
-    // @TODO: add system to take file from docs folder
+    /*
+    Code below is the possibility to use the api docs in nodejs.org repo
     const doc = await getDoc(process.env.DOCS_VERSIONS || 'v.20.x', params.slug);
     const RawContent = await fetch(doc.download_url).then((res) => res.text());
+    */
 
-	const MdxContent = await createMarkdownParser(
-        RawContent,
-            {
-              name: params.slug,
-              version: 'v16.0.0',
-              fullVersion: 'v16.0.0',
-              downloadUrl: doc.download_url,
-            }
-          );
+    // This is the possibility to use the api docs in nodejs/node repo (cannot be tested here)
+    const basePath = path.join(process.cwd(), 'doc', 'api');
+    const RawContent = getContentBySlug(params.slug, false, 'md', basePath);
+    const branch = process.env.DOCS_VERSIONS || 'v20.x';
+    const download_url = `https://raw.githubusercontent.com/nodejs/node/${branch}/doc/api/${params.slug}.md`;
     
-    // @TODO: add MDX parser
+    if(!RawContent) {
+        return null;
+    }
+    
+
+	const { parseMarkdown } = createMarkdownParser(
+        RawContent,
+        {
+            name: params.slug,
+            version: 'v16.0.0',
+            fullVersion: 'v16.0.0',
+            downloadUrl: download_url,
+        }
+    );
+
+    const { content } = await compileMDX({
+        source: await parseMarkdown(),
+        options: {
+            parseFrontmatter: true,
+        }
+    });
 
     return(
         <main>
-            
+            {content}
         </main>
     );
 };
--- packages/core/lib/contentManager.ts
+++ packages/core/lib/contentManager.ts
@@ -1,18 +1,19 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-const basePath = path.join(process.cwd(), '../../', 'content');
-
 function getContentBySlug(
 	slug: string,
-	lang?: string,
+	lang?: string | undefined,
 	ext: string = 'md',
+	basePath: string = path.join(process.cwd(), '../../', 'content')
 ): string | undefined {
 	try {
-		const filePath = path.join(basePath, `${slug}.${lang}.${ext}`);
-		return fs.readFileSync(filePath, 'utf8');
+		const filePath = lang ? `${slug}.${lang}.${ext}` : `${slug}.${ext}`;
+		const contentPath = path.join(basePath, filePath);
+		return fs.readFileSync(contentPath, 'utf8');
 	} catch (e) {
 		try {
+			if (!lang) return undefined;
 			const defaultFilePath = path.join(basePath, `${slug}.en.${ext}`);
 			return fs.readFileSync(defaultFilePath, 'utf8');
 		} catch (e) {
